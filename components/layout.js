2ad6e68e8b1a2a51c2f5f4b473ea4f6687eeb670
jordan <jvh78720@gmail.com>
2021-01-12 17:54:54 +0100

    Mise en place du menu burger

--- components/layout.js
+++ components/layout.js
@@ -1,15 +1,28 @@
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { slide as Menu } from "react-burger-menu";
+import SideBar from "./sidebar";
+
+
 
 export default function Layout({ children }) {
     return (
         <>
             <nav className="layoutNav">
-                <p>Bernard Aurélien Elagage</p>
-                <p>06 77 74 53 99</p>
-                <p>aurelien.78@orange.fr</p>
+                <div className="containerBurgerMenu">
+                    <div className="infoHeader">
+                        <p>Bernard Aurélien Elagage</p>
+                        <p>06 77 74 53 99</p>
+                        <p>aurelien.78@orange.fr</p>
+                    </div>
+                    <div id="App">
+                        <SideBar pageWrapId={"page-wrap"} outerContainerId={"App"} />
+
+                        <div id="page-wrap">
+                            {children}
+                        </div>
+                    </div>
+                </div>
             </nav>
-            <div className="containerLayout">
-                {children}
-            </div>
         </>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,16 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import Layout from '../components/layout';
-import Presentation from '../components/presentation'
-import CarouselAvis from '../components/avis'
-import Devis from '../components/devis'
-import Price from '../components/price'
-import Elagage from '../components/elagage'
-import Slider from '../components/slider'
+import Presentation from '../components/presentation';
+import CarouselAvis from '../components/avis';
+import Devis from '../components/devis';
+import Price from '../components/price';
+import Elagage from '../components/elagage';
+import Slider from '../components/slider';
 import Banniere1 from '../components/banniere1';
 import Banniere2 from '../components/banniere2';
+import Abattage from '../components/abattage';
+import TailleDeHaie from '../components/tailleDeHaie';
 
 
 export default function Home() {
@@ -28,6 +30,8 @@ export default function Home() {
         <Banniere1 />
         <Elagage />
         <Banniere2 />
+        <Abattage />
+        <TailleDeHaie />
         {/* <Slider items={[
           <img src='/photo/abattage.jpeg' />,
           <img src='/photo/démontage2.jpeg' />,
