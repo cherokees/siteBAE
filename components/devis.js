be98587e2e0d73cfb2482b26f02dbd144b4fe6ba
jordan <jvh78720@gmail.com>
2021-01-12 18:50:40 +0100

    design du composent devis avancé

--- components/devis.js
+++ components/devis.js
@@ -8,11 +8,13 @@ export default function Devis() {
                 <p className="catchphrase">Le meilleur rapport qualité prix, des offres à la carte, détaillées et adaptables.</p>
                 <h2>Vos travaux en 3 étapes</h2>
                 <h3>Par mail ou téléphone</h3>
+                <img src="./photo/phone.jpg" alt="téléphone" />
                 <div className="ItemDevis">
                     <HiOutlineArrowNarrowRight className="iconDevis" />
                     <p>Faire une demande pour un devis et une prise de rendez-vous sur place afin de comprendre vos besoins.</p>
                 </div>
                 <h3>Réalisation du devis</h3>
+                <img src="./photo/estimate.jpg" alt="téléphone" />
                 <div className="ItemDevis">
                     <HiOutlineArrowNarrowRight className="iconDevis" />
                     <p>Envoi d'une offre personnalisée par mail sous 8 jours</p>
@@ -22,6 +24,7 @@ export default function Devis() {
                     <p>Dès confirmation de votre accord par un simple mail ou appel téléphonique, vous recevrez une date approximative pour la réalisation des travaux.</p>
                 </div>
                 <h3>Réalisation des travaux</h3>
+                <img src="./photo/work.jpg" alt="téléphone" />
                 <div className="ItemDevis">
                     <HiOutlineArrowNarrowRight className="iconDevis" />
                     <p>Signature du devis et confirmation de la date définitive 15 jours avant le début des travaux.</p>
